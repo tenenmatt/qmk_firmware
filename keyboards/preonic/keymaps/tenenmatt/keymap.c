7ec8be39087e30772b41ed2b4507d94c19fdd757
Matt Tenenbaum <matt.tenenbaum@rockyou.com>
2023-12-30 01:03:59 -0500

    Add one-shot to right shift (dvorak only)

--- keyboards/preonic/keymaps/tenenmatt/keymap.c
+++ keyboards/preonic/keymaps/tenenmatt/keymap.c
@@ -47,6 +47,7 @@
  * home-row mods?
  * colemak will require '-' someplace (??)
  * (swap back to "just" KC_ESC rather than mod-tap ctl if home-row mods)
+ * bring colemak up to date with dvorak mods
  */
 
 enum preonic_layers {
@@ -86,7 +87,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   KC_GRV,         KC_1,           KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_DEL,
   KC_TAB,         KC_QUOT,        KC_COMM, KC_DOT,  KC_P,    KC_Y,    KC_F,    KC_G,    KC_C,    KC_R,    KC_L,    KC_BSPC,
   LCTL_T(KC_ESC), LT(_NUM, KC_A), KC_O,    KC_E,    KC_U,    KC_I,    KC_D,    KC_H,    KC_T,    KC_N,    KC_S,    KC_MINS,
-  OSM(MOD_LSFT),  KC_SCLN,        KC_Q,    KC_J,    KC_K,    KC_X,    KC_B,    KC_M,    KC_W,    KC_V,    KC_Z,    KC_RSFT,
+  OSM(MOD_LSFT),  KC_SCLN,        KC_Q,    KC_J,    KC_K,    KC_X,    KC_B,    KC_M,    KC_W,    KC_V,    KC_Z,    OSM(MOD_RSFT),
   XXXXXXX,        KC_LCTL,        KC_LALT, KC_LGUI, LOWER,   KC_SPC,  KC_SPC,  RAISE,   KC_RGUI, KC_RALT, KC_RCTL, KC_ENT
 ),
 
